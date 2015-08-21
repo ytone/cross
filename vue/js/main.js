f58c2386458f7e481915c91e1bec956e9aded73a
Yuta Tonegawa <y.tonegawa@raksul.com>
2015-08-21 17:09:35 +0900

    遊べるようにした

--- vue/js/main.js
+++ vue/js/main.js
@@ -1,12 +1,16 @@
-var table = [];
 var size = 3;
-var cols = [];
-for (var i = 0; i < size; i++) {
-    cols = [];
-    for (var j = 0; j < size; j++) {
-        cols.push({value: ""});
+
+function getFirstTable() {
+    var table = [];
+    var cols = [];
+    for (var i = 0; i < size; i++) {
+        cols = [];
+        for (var j = 0; j < size; j++) {
+            cols.push({value: ""});
+        }
+        table.push({cols: cols});
     }
-    table.push({cols: cols});
+    return table;
 }
 
 var cross = new Vue({
@@ -15,7 +19,7 @@ var cross = new Vue({
         turn: "O",
         turnList: ["O", "X"],
         turnIndex: 1,
-        rows: table,
+        rows: getFirstTable(),
     },
     methods: {
         onClick: function (e) {
@@ -24,32 +28,81 @@ var cross = new Vue({
                 return false;
             }
             e.targetVM.value = cross.$data.turn;
-            cross.$data.turn = cross.$data.turnList[cross.$data.turnIndex % cross.$data.turnList.length];
             if (cross.decide() === true) {
-                alert('You Win');
+                alert(cross.$data.turn + ' Win');
                 return false;
             }
+            cross.$data.turn = cross.$data.turnList[cross.$data.turnIndex % cross.$data.turnList.length];
             cross.$data.turnIndex++;
         },
 
         //勝ち負け判定
         decide: function () {
-            //横
-            for (var r = 0; r < cross.$data.rows.length; r++) {
-                var cols = cross.$data.rows[r].cols;
-                for (var i = 1; i < cols.length; i++) {
-                    if (cols[0].value != cols[i].value) {
-                        return false;
-                    }
+            var detectArray = [];
+            // 縦
+            for (var c = 0; c < size; c++) {
+                var colValue = [];
+                for (var r = 0; r < size; r++) {
+                    colValue.push(cross.$data.rows[r].cols[c].value);
                 }
-                return true;
+                detectArray.push(colValue);
             }
+
+            // 横
+            for (var r = 0; r < size; r++) {
+                var cols = cross.$data.rows[r].cols;
+                colValue = [];
+                for (var c = 0; c < size; c++) {
+                    colValue.push(cols[c].value);
+                }
+                detectArray.push(colValue);
+            }
+
+            // 斜め左上から右下
+            colValue = [];
+            for (var i = 0; i < size; i++) {
+                colValue.push(cross.$data.rows[i].cols[i].value);
+            }
+            detectArray.push(colValue);
+
+            // 斜め右上から左下
+            colValue = [];
+            var j;
+            for (var i = 0; i < size; i++) {
+                j = size - i - 1;
+                colValue.push(cross.$data.rows[i].cols[j].value);
+            }
+            detectArray.push(colValue);
+
+            // 3連続か判定
+            for (var i = 0; i < detectArray.length; i++) {
+                if (cross.isSame(detectArray[i]) == true) {
+                    return true;
+                }
+            }
+
+            return false;
+        },
+
+        isSame: function (arr) {
+            for (var i = 0; i < arr.length; i++) {
+                if (arr[i] == "") {
+                    return false;
+                }
+            }
+            for (var i = 1; i < arr.length; i++) {
+                if (arr[i] != arr[0]) {
+                    return false;
+                }
+            }
+
+            return true;
         },
 
         reset: function () {
             cross.$data.turnIndex = 1;
-            cross.$data.turn = cross.$data.turnList[cross.$data.turnIndex];
-            cross.$data.rows = table;
-        }
+            cross.$data.turn = cross.$data.turnList[0];
+            cross.$data.rows = getFirstTable();
+        },
     }
 });
